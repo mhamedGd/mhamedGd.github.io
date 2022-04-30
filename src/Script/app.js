be729c10cf0aadb373ff50656486f8af1f00b331
mhamedgd <41286935+mhamedGd@users.noreply.github.com>
2022-04-30 22:02:22 +0300

    F

---
+++ src/Script/app.js
@@ -0,0 +1,11 @@
+function scrollTo(){                  
+    window.alert("D");/*
+    var element = document.getElementById("text");
+    element.scrollIntoView(true);
+
+    var scrolledY = window.scrollY;
+
+    if(scrolledY){
+        window.scroll(0, scrolledY);
+    }     */           
+} 
